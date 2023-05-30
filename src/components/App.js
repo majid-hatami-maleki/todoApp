db56ee66bca1930b3083b6bf13a72928d07cbed7
majid hatami maleki <HH.kral@outlook.com>
2023-05-30 13:30:57 +0330

    starting

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './app.css'
+function App() {
+    return (
+        <div className='app-container'>
+            <div className="wrapper">
+
+            </div>
+        </div>
+    )
+}
+
+export default App
