623774624e99fee6cbe3f31b88ea36685aab52ff
majid hatami maleki <HH.kral@outlook.com>
2023-05-30 23:42:11 +0330

    populating and adding new-todo-panel to App component

--- src/components/create-todo-panel/newTodoPanel.js
+++ src/components/create-todo-panel/newTodoPanel.js
@@ -2,7 +2,18 @@ import React from 'react'
 import './newTodoPanel.css'
 function newTodoPanel() {
     return (
-        <div>newTodoPanel</div>
+        <div className='newTodoPanel-container'>
+            <form onSubmit={(e) => { e.preventDefault() }}>
+                <label>
+                    title
+                    <input type="text" placeholder='set a title ...' />
+                </label>
+                <label>
+                    description
+                    <textarea name="new-todo-description-input" id="" cols="30" rows="10"></textarea>
+                </label>
+            </form>
+        </div>
     )
 }
 
