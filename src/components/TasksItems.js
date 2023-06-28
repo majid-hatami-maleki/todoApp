e791d0094af00d4f258ae155e19e003564eb621a
majid hatami maleki <HH.kral@outlook.com>
2023-06-28 10:38:24 +0330

    make the project responsive

--- src/components/TasksItems.js
+++ src/components/TasksItems.js
@@ -1,38 +1,56 @@
-import React, { useContext } from 'react'
+import React, { useContext, useMemo } from 'react'
 import './tasksItems.css'
 import { MdDeleteOutline, MdDone } from 'react-icons/md'
 import AnimatedBtns from './AnimatedBtns'
 import { tasksContext } from '../contexts/tasksContext'
 
 export default function TasksItems(props) {
-    const { title, description, id } = props
+    const { id, title, description, isDone } = props
     const contextData = useContext(tasksContext)
     return (
         <>
-            <li className='active-tasks-list-items'>
-
-                <div className="tasks-list-items-texts">
-
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-
-                </div>
-
-                <div className="tasks-list-items-options">
-
-                    <AnimatedBtns
-                        title={'remove'}
-                        icon={<MdDeleteOutline />}
-                        func={() => { contextData.removeTaskHandler(id) }}
-                    />
-
-                    <AnimatedBtns
-                        title={'done'}
-                        icon={<MdDone />}
-                    />
-
-                </div>
-            </li>
+            {useMemo(() => {
+                return !isDone ? (
+                    <li className='active-tasks-list-items'>
+                        <div className="tasks-list-items-texts">
+                            <h2>{title}</h2>
+                            <p>{description}</p>
+                        </div>
+                        <div className="tasks-list-items-options">
+                            <AnimatedBtns
+                                title={'remove'}
+                                icon={<MdDeleteOutline />}
+                                func={() => { contextData.removeTaskHandler(id) }}
+                            />
+                            <AnimatedBtns
+                                title={'done'}
+                                icon={<MdDone />}
+                                func={() => { contextData.doneTaskHandler(id) }}
+                            />
+                        </div>
+                    </li>
+                ) : (
+                    <li className='deActive-tasks-list-items'>
+                        <div className="tasks-list-items-texts">
+                            <h2>{title}</h2>
+                            <p>{description}</p>
+                        </div>
+                        <div className="tasks-list-items-options">
+                            <AnimatedBtns
+                                title={'remove'}
+                                icon={<MdDeleteOutline />}
+                                func={() => { contextData.removeTaskHandler(id) }}
+                            />
+                            <AnimatedBtns
+                                title={'done'}
+                                icon={<MdDone />}
+                                func={() => { contextData.doneTaskHandler(id) }}
+                            />
+                        </div>
+                    </li>
+                )
+            }, [contextData.allTasks])
+            }
         </>
     )
 }
--- src/contexts/tasksContext.js
+++ src/contexts/tasksContext.js
@@ -4,12 +4,7 @@ const tasksContext = createContext()
 
 
 function TasksContextProvider({ children }) {
-    const [allTasks, setAllTasks] = useState([
-        { id: 1, title: 'html', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officia magni culpa at quam alias eos in harum, id dolores debitis voluptatem, soluta animi ratione laboriosam amet repellendus. Provident, voluptatem placeat reiciendis totam eum dolores sequi dicta odit voluptatibus, autem veniam exercitationem maiores iusto dolorem suscipit vero, molestias cumque omnis adipisci ullam facere! Amet officia modi dolores ipsa incidunt impedit architecto harum aperiam voluptatem, deleniti exercitationem officiis nisi quo magni recusandae soluta, delectus explicabo. Amet omnis tempora, tempore minus aspernatur accusantium? Deserunt corporis, perspiciatis, accusamus omnis quis natus sunt reiciendis nulla laborum cupiditate molestiae provident cum autem delectus sit maxime.' },
-        { id: 2, title: 'js project', description: 'working on Javascript todo project' },
-        { id: 3, title: 'css', description: 'do the project responsive' },
-        { id: 4, title: 'html', description: 'learn html' },
-    ])
+    const [allTasks, setAllTasks] = useState([])
     const [newTaskTitle, setNewTaskTitle] = useState('')
     const [newTaskDescription, setNewTaskDescription] = useState('')
 
@@ -17,6 +12,10 @@ function TasksContextProvider({ children }) {
         const removeItem = allTasks.filter((item) => { return item.id !== id })
         setAllTasks(removeItem)
     }
+    function doneTaskHandler(id) {
+        setAllTasks(allTasks.map(item => item.id === id ? { ...item, isDone: true } : item));
+    }
+    // { data: this.state.data.map(el => (el.id === id ? { ...el, text } : el)) }
     return (
         <tasksContext.Provider value={{
             allTasks,
@@ -26,6 +25,7 @@ function TasksContextProvider({ children }) {
             newTaskDescription,
             setNewTaskDescription,
             removeTaskHandler,
+            doneTaskHandler
         }}>
             {children}
         </tasksContext.Provider>
