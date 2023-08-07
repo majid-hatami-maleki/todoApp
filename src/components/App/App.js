9f202d126dad424def7df31f5d5dd92eed4534b3
majid hatami maleki <HH.kral@outlook.com>
2023-08-07 17:45:05 +0330

    programming the app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './app.css'
+import {MdDone , MdOutlineRemove , MdModeEdit} from 'react-icons/md'
 export default function App() {
   return (
     <div className='app-container'>
         <h1 className='app-title'>TODO APP</h1>
         <div className='todo-app-container'>
-            <ul>
-                <li></li>
+            <ul className='todo-app-display'>
+                <li>
+                    <p>item 1</p>
+                    <div className="todo-item-options">
+                        <button title='remove'><MdOutlineRemove/></button>
+                        <button title='Done'><MdDone/></button>
+                        <button title='Edit'><MdModeEdit/></button>
+                    </div>
+                </li>
             </ul>
             <div className="create-todo-panel">
                 <input type="text" />
