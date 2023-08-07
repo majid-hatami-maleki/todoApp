8b9fd1318df357173fc8b87a14abf377416da009
majid hatami maleki <HH.kral@outlook.com>
2023-08-07 17:28:34 +0330

    populating and styling todos li elems

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,16 @@ import './app.css'
 export default function App() {
   return (
     <div className='app-container'>
-        <form></form>
+        <h1 className='app-title'>TODO APP</h1>
+        <div className='todo-app-container'>
+            <ul>
+                <li></li>
+            </ul>
+            <div className="create-todo-panel">
+                <input type="text" />
+                <button>add</button>
+            </div>
+        </div>
     </div>
   )
 }
