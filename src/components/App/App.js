ad7454a14fec6f64dea14b3306441d137491c865
majid hatami maleki <HH.kral@outlook.com>
2023-08-09 23:27:27 +0330

    programming each todo Done BTN

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,40 +1,57 @@
 import React, { useMemo, useState } from 'react'
 import './app.css'
-import {MdDone , MdOutlineRemove , MdModeEdit} from 'react-icons/md'
+import { MdDone, MdOutlineRemove, MdModeEdit } from 'react-icons/md'
 export default function App() {
-    const [inputValue , setInputValue] = useState('')
+    const [inputValue, setInputValue] = useState('')
     const [allTodosArray, setAllTodosArray] = useState([])
-    const newObj = new Object()
-  return (
-    <div className='app-container'>
-        <h1 className='app-title'>TODO APP</h1>
-        <div className='todo-app-container'>
-        <ul className='todo-app-display'>
-            {useMemo(()=>{
-                 return  allTodosArray.map((todo)=>{
-                  return  <li>
-                    <p>{todo.value}</p>
-                    <div className="todo-item-options">
-                        <button title='remove'><MdOutlineRemove/></button>
-                        <button title='Done'><MdDone/></button>
-                        <button title='Edit'><MdModeEdit/></button>
-                    </div>
-                </li>
-                })
-            },[allTodosArray])}
-            </ul>
-            <div className="create-todo-panel">
-                <input type="text" value={inputValue} onChange={(e)=>{setInputValue(e.target.value)}}/>
-                <button onClick={()=>{
-                    newObj.id = allTodosArray.length + 1
-                    newObj.value = inputValue
-                    setAllTodosArray(prevState => {
-                        return [...prevState , newObj]
-                })
-                    setInputValue('')
-                }}>add</button>
+    const todoClickHandler = (e) => {
+        const newObj = new Object()
+        newObj.id = allTodosArray.length + 1
+        newObj.value = inputValue
+        setAllTodosArray(prevState => {
+            return [...prevState, newObj]
+        })
+        setInputValue('')
+    }
+    const removeTodoHandler = (item)=>{
+        const filteredItem = allTodosArray.filter((todo)=> todo.id !== item.id)     
+        setAllTodosArray([...filteredItem])
+    }
+    return (
+        <div className='app-container'>
+            <h1 className='app-title'>TODO APP</h1>
+            <div className='todo-app-container'>
+                <ul className='todo-app-display'>
+                    {useMemo(() => {
+                        return allTodosArray.map((todo) => {
+                            return <li key={todo.id}>
+                                <p>{todo.value}</p>
+                                <div className="todo-item-options">
+                                    <button
+                                        title='remove'
+                                        onClick={()=>{removeTodoHandler(todo)}}
+                                    >
+                                        <MdOutlineRemove />
+                                    </button>
+                                    <button title='Done'><MdDone /></button>
+                                    <button title='Edit'><MdModeEdit /></button>
+                                </div>
+                            </li>
+                        })
+                    }, [allTodosArray])}
+                </ul>
+                <div className="create-todo-panel">
+                    <input type="text" value={inputValue}
+                        onChange={(e) => { setInputValue(e.target.value) }}
+                        onKeyDown={(e) => {
+                            if (e.code === 'Enter') todoClickHandler()
+                        }}
+                    />
+                    <button
+                        onClick={(e) => { todoClickHandler() }}
+                    >add</button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
